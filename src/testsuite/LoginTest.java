3b2401ed93a699efe483c70ac7a72420a7c14d9d
htpatel1993 <htpatel1993@gmail.com>
2023-04-25 17:07:29 +0100

    project 04

---
+++ src/testsuite/LoginTest.java
@@ -0,0 +1,61 @@
+package testsuite;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.Utility;
+
+import java.util.List;
+
+public class LoginTest extends Utility {
+    String baseUrl = "https://www.saucedemo.com/";
+
+    @Before
+    public void setUp() {
+        // Open browser and launch url
+        openBrowser(baseUrl);
+    }
+
+    @After
+    public void tearDown() {
+        //Close all browser
+        closeBrowser();
+    }
+
+    @Test
+    public void userSholdLoginSuccessfullyWithValid() {
+        // Enter “standard_user” username
+        sendTextToElement(By.xpath("//input[@id='user-name']"), "standard_user");
+        // Enter “secret_sauce” password
+        sendTextToElement(By.xpath("//input[@id='password']"), "secret_sauce");
+        //Click on ‘LOGIN’ button
+        clickOnElement(By.xpath("//input[@id='login-button']"));
+        //Verify the text “PRODUCTS”
+        String expectedMessage = "Products";
+        String actualMessage = getTextFromElement(By.xpath("//span[@class='title']"));
+        verifyTwoTextMessage(expectedMessage, actualMessage);
+
+    }
+
+    @Test
+    public void verifyThatSixProductsAreDisplayedOnPage() {
+
+        // Enter “standard_user” username
+        sendTextToElement(By.xpath("//input[@id='user-name']"), "standard_user");
+        // Enter “secret_sauce” password
+        sendTextToElement(By.xpath("//input[@id='password']"), "secret_sauce");
+        //Click on ‘LOGIN’ button
+        clickOnElement(By.xpath("//input[@id='login-button']"));
+
+        // Verify that six products are displayed on page
+        int expectedMessage = 6;
+        List<WebElement> totalproduct = driver.findElements(By.xpath("//div[@class='inventory_item']"));
+        int accepted = totalproduct.size();
+        verifyTwoTextMessage(expectedMessage, accepted);
+
+    }
+
+
+}
